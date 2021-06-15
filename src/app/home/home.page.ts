3248995ab1359a78b57c0ec339be29613594933a
Ken Sodemann <ken@ionic.io>
2021-06-15 09:00:41 -0500

    add the segment

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedSegment: string;
 
   constructor() {}
 
+  refreshChart() {
+    console.log('change', this.selectedSegment);
+  }
 }
